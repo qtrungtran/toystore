e576406e35b44c24c7f508825a046db81a2efd67
Trung Tran <trung_tran@datahouse.com>
2021-05-30 23:39:28 +0700

    update paypal

--- controllers/CheckoutController.js
+++ controllers/CheckoutController.js
@@ -5,14 +5,9 @@ const getUserInfo = require("../utils/getUserInfo");
 const paypal = require("@paypal/payouts-sdk");
 class CheckoutController {
 	async payout(req, res) {
-		let clientId =
-			"AahRbFwBxZbe_2LApCXd-8j3eyIuwbBEqM0qU6Jdmey7HPU_RHxCkCl_1MDZCxQVZAJgJPr9rsmseJs7";
-		let clientSecret =
-			"ELLm2UwloV_QeRoHqeEVIUGhQccoeGbLoWeotz8xhcizymtRAVjpfLgJQypk1AkYC4S9FRDLFQA0gK4S";
-		let environment = new paypal.core.SandboxEnvironment(
-			clientId,
-			clientSecret
-		);
+		let clientId = "AQAbunPoq-8ZpQ_rIzKBO96RfBgjXEtjzTKsk3hbCrTp5Bf4P2yZMbsMunmwZOYBDf24lA2ICfTitROj";
+		let clientSecret = "EL2Q-3on6Tq3yBGeAJLCAUKiseejV8SXYsIeitUdGO-TNvoFCB2dAnFvJ4Q0PBbZj6WjgDSL3ZFRXoyt";
+		let environment = new paypal.core.SandboxEnvironment(clientId, clientSecret);
 		let client = new paypal.core.PayPalHttpClient(environment);
 
 		const now = new Date();
@@ -24,19 +19,19 @@ class CheckoutController {
 				email_message: "SDK payouts test txn",
 				note: "Enjoy your Payout!!",
 				sender_batch_id: key,
-				email_subject: "This is a test transaction from SDK"
+				email_subject: "This is a test transaction from SDK",
 			},
 			items: [
 				{
 					note: "Your 5$ Payout!",
 					amount: {
 						currency: "USD",
-						value: amount
+						value: amount,
 					},
-					receiver: "myuser@personal.example.com",
-					sender_item_id: key
-				}
-			]
+					receiver: "sb-pnsqh6362630@business.example.com",
+					sender_item_id: key,
+				},
+			],
 		};
 
 		// Construct a request object and set desired parameters
@@ -49,9 +44,7 @@ class CheckoutController {
 			let response = await client.execute(request);
 			console.log(`Response: ${JSON.stringify(response)}`);
 			// If call returns body in response, you can get the deserialized version from the result attribute of the response.
-			console.log(
-				`Payouts Create Response: ${JSON.stringify(response.result)}`
-			);
+			console.log(`Payouts Create Response: ${JSON.stringify(response.result)}`);
 			return response.result.batch_header.payout_batch_id;
 		};
 		const r = await createPayouts();
